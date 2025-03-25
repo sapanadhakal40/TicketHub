60cb094fb511e55c220c9932008c4645f8cd0ca0
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-03-25 12:13:15 +0200

    Configure nextjs build settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
