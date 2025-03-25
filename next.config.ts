5e8b99fc5cca49dcbfaaef62690aefc05d02121b
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-03-25 12:22:33 +0200

    update next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  }
+
 };
 
 export default nextConfig;
